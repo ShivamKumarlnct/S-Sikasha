83c879666f90724d256fb8731344d0b2b1dc2a45
Shivam Kumar <135052604+ShivamKumarlnct@users.noreply.github.com>
2025-12-27 11:39:48 +0530

    comit link english

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
             <Link to="https://interview-master-ai-ffc80435.base44.app" onClick={toggleNavbar}> Interview Practice</Link>
           </li>
            <li className='navlist'>
-            <Link to="https://lingua-connect-88e36417.base44.app" onClick={toggleNavbar}> Communication Practice</Link>
+            <Link to="https://sikasha.base44.app" onClick={toggleNavbar}> Communication Practice</Link>
           </li>
           
         </ul>
